459f420b3bbc4064548e033100430f001a1db3c5
Fayyaz24 <adi.sumardi808@gmail.com>
2024-11-19 12:47:03 +0700

    membuat api

--- app/Filament/Pages/Map.php
+++ app/Filament/Pages/Map.php
@@ -4,9 +4,9 @@ namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
 
-class Map extends Page
-{
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+// class Map extends Page
+// {
+//     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
-    protected static string $view = 'filament.pages.map';
-}
+//     protected static string $view = 'filament.pages.map';
+// }
